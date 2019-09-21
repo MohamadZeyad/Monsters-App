9f3bd28d5b50ae5ed029413f0bb2416c0011a3bb
Mohamad Zeyad <m-store@MStoresMacBook.Home>
2019-09-22 01:31:43 +0300

    Updates

---
+++ precache-manifest.86d4de19a8ae0b3e67d790094e122ba6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da191960e200fdfb7b5afa3e798a9b73",
+    "url": "/Monsters-App/index.html"
+  },
+  {
+    "revision": "7564078730713f29983b",
+    "url": "/Monsters-App/static/css/main.f019197c.chunk.css"
+  },
+  {
+    "revision": "3ee7a2ed19d891ee036c",
+    "url": "/Monsters-App/static/js/2.f2345ca0.chunk.js"
+  },
+  {
+    "revision": "7564078730713f29983b",
+    "url": "/Monsters-App/static/js/main.15269afd.chunk.js"
+  },
+  {
+    "revision": "ed33b22315ad7b6e3397",
+    "url": "/Monsters-App/static/js/runtime-main.dcb25343.js"
+  }
+]);
