37fed0e5d0b6514c21027107f5644e169fa88100
Mohamad Zeyad <m-store@MStoresMacBook.Home>
2019-09-23 00:25:52 +0300

    testing

--- src/App.js
+++ src/App.js
@@ -32,7 +32,7 @@ class App extends React.Component {
     );
     return (
       <div className="App">
-        <h1>Monsters</h1>
+        <h1>Monsters Rolodex</h1>
         {/* <input
           type="search"
           placeholder="search monsters"
